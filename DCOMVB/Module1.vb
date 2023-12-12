8fcf053979a2dda2b1f46376c36fb91c30d30b3d
Martin Cortez <i20050008@monclova.tecnm.mx>
2023-12-12 15:01:45 -0600

    Initial commit

---
+++ DCOMVB/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.InteropServices
+
+Namespace DCOMVB
+    <ComVisible(True)>
+    Public Class ServidorDCOM
+        <ComVisible(True)>
+        Public Function ObtenerSaludo(nombre As String) As String
+            Return $"Hola, {nombre}!"
+        End Function
+    End Class
+End Namespace
+
